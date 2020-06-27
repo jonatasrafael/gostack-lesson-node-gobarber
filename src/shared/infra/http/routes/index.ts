9adb03f25cfaeac9202bb9e0cc69ddd37e585e3f
Jonatas Rafael Carneiro Ribeiro <jonatasrafael@jonatasrafael.com>
2020-06-26 23:42:47 -0300

    feat: added profile managing

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 
 const routes = Router();
 
@@ -10,5 +12,6 @@ routes.use('/sessions', sessionsRouter);
 routes.use('/users', usersRouter);
 routes.use('/appointments', appointmentsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
