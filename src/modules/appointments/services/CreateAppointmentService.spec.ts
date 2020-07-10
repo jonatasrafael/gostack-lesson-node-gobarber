d6e25ba3189dda0cd10672f6429faa7c94a94da2
Jonatas Rafael Carneiro Ribeiro <jonatasrafael@jonatasrafael.com>
2020-07-10 01:00:47 -0300

    feat: can only create appointments between 8am and 5pm

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,5 +1,7 @@
 import AppError from '@shared/errors/AppError';
 
+import { addDays } from 'date-fns';
+
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -21,33 +23,39 @@ describe('CreateAppointment', () => {
 
     const appointment = await createAppointment.execute({
       date: new Date(2020, 4, 10, 13),
-      user_id: '123123',
-      provider_id: '123123',
+      user_id: 'user_id',
+      provider_id: 'provider_id',
     });
 
     expect(appointment).toHaveProperty('id');
-    expect(appointment.provider_id).toBe('123123');
+    expect(appointment.provider_id).toBe('provider_id');
   });
 
   it('should not be able to create two appointments on the same time', async () => {
-    const appointmentDate = new Date(Date.now());
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
 
     await createAppointment.execute({
-      date: appointmentDate,
-      user_id: '123123',
-      provider_id: '123123',
+      date: new Date(2020, 4, 10, 13),
+      user_id: 'user_id',
+      provider_id: 'provider_id',
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
     });
 
     await expect(
       createAppointment.execute({
-        date: appointmentDate,
-        user_id: '123123',
-        provider_id: '123123',
+        date: new Date(2020, 4, 10, 13),
+        user_id: 'user_id',
+        provider_id: 'provider_id',
       }),
-    ).rejects.toBeInstanceOf(AppError);
+    ).rejects.toEqual(new AppError('This appointment is already booked'));
   });
 
-  it('should not be able to create an appointments on a past date', async () => {
+  it('should not be able to create an appointment on a past date', async () => {
     jest.spyOn(Date, 'now').mockImplementationOnce(() => {
       return new Date(2020, 4, 10, 12).getTime();
     });
@@ -55,9 +63,55 @@ describe('CreateAppointment', () => {
     await expect(
       createAppointment.execute({
         date: new Date(2020, 4, 10, 11),
-        user_id: '123123',
-        provider_id: '123123',
+        user_id: 'user_id',
+        provider_id: 'provider_id',
       }),
     ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment with same user as provider', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 10, 13),
+        user_id: 'user_id',
+        provider_id: 'user_id',
+      }),
+    ).rejects.toEqual(
+      new AppError("You can't create an appointment with yourself."),
+    );
+  });
+
+  it('should not be able to create an appointment before 8am and after 5pm', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 11, 7),
+        user_id: 'user_id',
+        provider_id: 'provider_id',
+      }),
+    ).rejects.toEqual(
+      new AppError('You can only create appointments between 8am and 5pm.'),
+    );
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2020, 4, 11, 18),
+        user_id: 'user_id',
+        provider_id: 'provider_id',
+      }),
+    ).rejects.toEqual(
+      new AppError('You can only create appointments between 8am and 5pm.'),
+    );
   });
 });
