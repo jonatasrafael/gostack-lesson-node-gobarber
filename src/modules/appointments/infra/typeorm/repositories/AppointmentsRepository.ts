8531e834054d129fd9ccdf16a4f261ca9318c01e
Jonatas Rafael Carneiro Ribeiro <jonatasrafael@jonatasrafael.com>
2020-08-02 01:15:10 -0300

    refactor: added relations user

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -42,6 +42,7 @@ class AppointmentsRepository implements IAppointmentRepository {
             `to_char(${dateFieldName}, 'DD-MM-YYYY') = '${parsedDay}-${parsedMonth}-${year}'`,
         ),
       },
+      relations: ['user'],
     });
 
     return appointments;
